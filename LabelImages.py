93c3b216ecfb72b3ef2a2f62d93dbd2c0fa7e09f
Aya <1220020@student.birzeit.edu>
2025-12-12 23:01:56 +0300

    Class to label images

---
+++ LabelImages.py
@@ -0,0 +1,18 @@
+import torch
+from torch.utils.data import Dataset
+from LSB_model import Convert_images_to_array_of_bit
+
+
+class LabelImages(Dataset):
+    def __init__(self, normal_paths, stego_paths):
+        self.image_paths = normal_paths + stego_paths
+        self.labels = [0]*len(normal_paths) + [1]*len(stego_paths)
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, idx):
+        # Get the bit  for the image
+        bit_tensor = Convert_images_to_array_of_bit(self.image_paths[idx])
+        label = self.labels[idx]
+        return bit_tensor, torch.tensor(label, dtype=torch.long)
